78fe9d627ef0b03e05235496faec3f590e3d5947
代轩 <1752837684@qq.com>
2025-06-02 01:30:47 +0800

    Update javdb.js

--- javdb.js
+++ javdb.js
@@ -1,16 +1,4 @@
-const url = $request.url;
-let header = $request.headers;
-
-if (url.includes("/api/v1/movies/") && url.includes("/play?")) {
-  header.authorization =
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MzE4OTE0NSwidXNlcm5hbWUiOiJjaHhtMTAyNSJ9.9biip2hb60jXeakBMbnP-5QiLyycLj9s7dpHyXNUp7E"
-  $done({ headers: header });
-} else {
-  $done({});
-}
-//
 if (!/html>/.test($response.body)) $done({});
-
 // 去顶部域名,底部下载提醒,播放页广告
 const body = $response.body.replace(
   /<\/head>/,
