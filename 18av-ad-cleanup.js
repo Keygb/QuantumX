f4e4daff16ea0eee37c2c48b97d5b0efad65bdd3
代轩 <1752837684@qq.com>
2025-06-04 22:55:22 +0800

    Update 18av-ad-cleanup.js

--- 18av-ad-cleanup.js
+++ 18av-ad-cleanup.js
@@ -3,19 +3,20 @@ let body = $response.body;
 body = body.replace(
   /<\/head>/i,
   `<style>
-    /* 隐藏被拦截广告图的占位容器 */
-    img[src*="/adcg/"],
+    /* 精确隐藏广告图（仅限 /adcg/ 广告目录） */
+    img[src^="/adcg/"],
     img[src*="mmfree3.com/A_PH/"] {
       display: none !important;
     }
 
-    div:has(img[src*="/adcg/"]),
-    div:has(img[src*="mmfree3.com/A_PH/"]),
-    a:has(img[src*="/adcg/"]),
-    a:has(img[src*="mmfree3.com/A_PH/"]),
-    table:has(img[src*="/adcg/"]),
-    table:has(img[src*="mmfree3.com/A_PH/"]),
-    iframe[src*="/adcg/"] {
+    /* 只隐藏典型广告块容器，保留播放页结构 */
+    div#ad_right,  /* 右侧广告 */
+    div#ad_left,   /* 左侧广告 */
+    div#ad_top,    /* 顶部广告 */
+    div#ad_bottom, /* 底部广告 */
+    iframe[src*="/adcg/"],
+    table:has(img[src^="/adcg/"]),
+    a[href*="/adcg/"] {
       display: none !important;
     }
   </style></head>`
